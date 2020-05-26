792b93e1f6a5bf927a5bba3e842dc2729ee0e3d0
Derek Strobel <derek@Ashitaka.localdomain>
2020-05-26 11:13:42 -0700

    lab 9 derek

---
+++ lab9_Derek/lab9_notes.md
@@ -0,0 +1,42 @@
+# Double-ended queue (deque) with a doubly linked list
+Derek Strobel  
+CIS212 University of Oregon  
+Spring 2020
+
+## Why deque?
+It's a pretty niche data structure. But for some problems, it's really handy. If you're interested, check out https://en.wikipedia.org/wiki/Work_stealing
+
+## How deque?
+Deque is just a queue where we can access *both* ends. You could kind of think of it like a queue AND a stack at the same time. 
+
+Like most data structures, you can implement it a lot of ways. Today we'll look at (in Python) a doubly-linked list implementation, which is pretty straightforward. However, it's NOT the only implementation, and indeed some implementations (like C++'s `std::deque`) work much differently internally. However, to the end user, they appear to be the same!
+
+## Doubly-Linked List
+Singly-linked lists: each node has a `next`
+Doubly-linked lists: each node has a `next` **and** a `previous`!
+
+Pros:
+* Can now traverse forwards and backwards!
+
+Cons:
+* Have to keep 2 pointers per node instead of one. Might not seem like much, but consider that it actually **doubles** the amount of memory overhead vs singly-linked list!
+
+## The Sentinel node
+
+![gandalf](https://www.icge.co.uk/languagesciencesblog/wp-content/uploads/2014/04/you_shall_not_pass1.jpg)
+
+Just a convenient way to keep track of the head and tail of the list!
+
+Sentinel node:
+* `next`: Head of list
+* `prev`: Tail of list
+
+When the list is empty, the sentinel's `next` and `prev` point back to the sentinel!
+
+Benefits:
+* No conditionals when pushing or popping nodes!
+* No conditionals when iterating!
+
+All we need is a way to tell the sentinel node apart from regular nodes.
+
+![Sentinel diagram](https://i.stack.imgur.com/nlX1F.png)
---
+++ lab9_Derek/lldeque.py
@@ -0,0 +1,76 @@
+class dllnode():
+    def __init__(self, value, next=None, prev=None, sentinel=False):
+        self.value = value
+        self.sentinel = sentinel
+        if sentinel:
+            self.next = self
+            self.prev = self
+        else:
+            self.next = next
+            self.prev = prev
+        
+    def __str__(self):
+        return f'dllnode({self.value})'
+
+class lldeque():
+    def __init__(self):
+        self.sentinel = dllnode(value=None, sentinel=True)
+        self.size = 0
+
+    def push_back(self, value):
+        newnode = dllnode(value, next=self.sentinel, prev=self.sentinel.prev)
+        self.sentinel.prev.next = newnode
+        self.sentinel.prev = newnode
+        self.size += 1
+    
+    def push_front(self, value):
+        newnode = dllnode(value, next=self.sentinel.next, prev=self.sentinel)
+        self.sentinel.next.prev = newnode
+        self.sentinel.next = newnode
+        self.size += 1
+    
+    def pop_front(self):
+        n = self.sentinel.next
+        n.next.prev = self.sentinel
+        self.sentinel.next = self.sentinel.next.next
+        return n.value
+        
+    def pop_back(self):
+        n = self.sentinel.prev
+        n.prev.next = self.sentinel
+        self.sentinel.prev = self.sentinel.prev.prev
+        return n.value
+    
+    def peek_front(self):
+        return self.sentinel.next.value
+    
+    def peek_back(self):
+        return self.sentinel.prev.value
+    
+    def __len__(self):
+        return self.size
+    
+    def __str__(self):
+        s = ''
+        k = self.sentinel.next
+        while k.sentinel == False:
+            s += str(k) + ', '
+            k = k.next
+        return s
+    
+if __name__ == '__main__':
+    d = lldeque()
+    print(d)
+    d.push_back(5)
+    print(d)
+    d.push_back(2)
+    print(d)
+    d.push_front(7)
+    print(d)
+    print(len(d))
+    print(d.pop_back())
+    print(d)
+    print(d.peek_front())
+    print(d)
+    print(d.pop_front())
+    print(d)
